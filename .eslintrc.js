10edcda6592a69587b0996af7d7be9f8fe1c088d
Thanhnhanle123 <nestalect65@gmail.com>
2024-03-12 21:38:35 +0700

    backend 2

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,8 @@
 module.exports = {
   env: {
-    node: true,
-    commonjs: true,
+    browser: true,
     es2021: true,
+    node: true,
   },
-  extends: ["eslint:recommended", "prettier"],
+  extends: ["eslint:recommended", "plugin:vue/vue3-essential"],
 };
